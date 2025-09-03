af97695498f674875a6faf67ddbed0f2b2b845e3
Lace Ronald <lace.m.ronald@gmail.com>
2025-09-03 12:28:27 -0400

    Start parsing file. Currently issue with how things are indexed. Need to look at later.

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # Main function for running data processing
 
-from misconduct import (data_report)
+from misconduct import (data_report, miscon_per_institution)
 import os
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    report = data_report('./', 'dbo_Miscon.csv')
+    report = data_report('./', 'dbo_Miscon.txt')
     print("Miscon opened")
-    print(report)
-
+    print(miscon_per_institution(report, '2020'))
--- misconduct.py
+++ misconduct.py
@@ -2,27 +2,36 @@ import pandas as pd
 import os
 import re
 import plotly.express as px
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import codecs
 
 
 """
 Author: Lace Ronald
-
+    
 Re-use of this code is permitted under the MIT license. Property of Bucknell University.
-
+    
 """
 
 def data_report(dirname, filename):
-    # Used to verify data is well-formed.
-    # Fix encoding error and convert to UTF-8 before reading file.
-    source_encoding = 'latin1'
-    source_file = os.path.join(dirname, filename)
-    target_file = os.path.join(dirname, filename) + '_utf8.txt'
-    with codecs.open(source_file, 'r', source_encoding) as infile:
-        with codecs.open(target_file, 'w', 'utf-8') as outfile:
-            outfile.write(infile.read())
-    fRead = pd.read_csv(target_file)
-    return fRead
+        # Used to verify data is well-formed.
+        # Fix encoding error and convert to UTF-8 before reading file.
+        source_encoding = 'latin1'
+        source_file = os.path.join(dirname, filename)
+        target_file = os.path.join(dirname, filename) + '_utf8.txt'
+        with codecs.open(source_file, 'r', source_encoding) as infile:
+            with codecs.open(target_file, 'w', 'utf-8') as outfile:
+                outfile.write(infile.read())
+        fRead = pd.read_csv(target_file)
+        return fRead
 
+# def write_file(obj_dict):
+#   Create a function here that will consume a python dictionary and output a csv file for easy read.
+
+def miscon_per_institution(data_report, year):
+        # Goes through report and creates a dictionary of misconduct counts by institution
+        # Filter for preferred year by misconduct_date
+        miscon_dict = {}
+        for miscon in data_report:
+            print(miscon)
