2ee1d8bb9323e8b1512f154a95d09a7fa6ed122e
Lace Ronald <lace.m.ronald@gmail.com>
2025-09-23 13:06:23 -0400

    In the middle of counting misconducts by month. Does not currenly compile. Need to fix key error.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Main function for running data processing
 from data_report import *
-from misconduct import (miscon_per_institution)
+from misconduct import *
 from population import *
 import os
 
@@ -9,9 +9,14 @@ if __name__ == '__main__':
     inst_miscon_report = data_report('./', 'dbo_Miscon.txt')
     misconduct = miscon_per_institution(inst_miscon_report, '2023')
 
+
     inst_pop_report_annual = data_report('./', 'physically-present-population-23-24.csv')
     population = population_per_institution(inst_pop_report_annual, '2023')
     annual_rates = rates_of_misconduct_per_year(misconduct, population)
+
+    inst_miscon_report_monthly = data_report('./', 'dbo_Miscon.txt')
+    miscon_by_month = miscon_per_institution_by_month_and_year(inst_miscon_report_monthly, '2023')
+    print("miscon by month: ", miscon_by_month)
 
     inst_pop_report_monthly = data_report('./', 'physically-present-population-23-24.csv')
     pop_by_month = population_per_institution_by_month_and_year(inst_pop_report_monthly, '2023')
--- misconduct.py
+++ misconduct.py
@@ -14,6 +14,8 @@ Re-use of this code is permitted under the MIT license. Property of Bucknell Uni
     
 """
 def miscon_by_month_and_year(data_report, month, year):
+        #need to rewrite to avoid filtering like this and match up with population function.
+        # TODO: fix this and then re-check compiling.
         data_report[year] = data_report['misconduct_date'].apply(lambda x: 1 if str(x)[:6] == (year + month) else 0)
         return data_report[(data_report[year] == 1)]
 
@@ -33,3 +35,20 @@ def miscon_per_institution(data_report, year):
                 else:
                         miscon_per_institution[mis_inst] += 1
         return miscon_per_institution
+
+def miscon_per_institution_by_month_and_year(data_report, year):
+    inst_data = miscon_by_year(data_report, year)
+    miscon_per_institution = {}
+    for inst in inst_data['institution']:
+        miscon_per_month = {}
+        month = 1
+        while month < 13:
+            month_str = str(month)
+            if month < 10:
+                month_str = "0" + month_str
+            data_by_month = miscon_by_month_and_year(inst_data, month_str, year)
+            for monthly_pop in data_by_month['FIX THIS']:
+                miscon_per_month[month_str] = monthly_pop
+            month += 1
+            miscon_per_institution[inst] = miscon_per_month
+    return miscon_per_institution
