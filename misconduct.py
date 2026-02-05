a7ea7b5460a7e5bb7b1e0bf1c26968835243be1a
Lace <lace.m.ronald@gmail.com>
2026-02-05 15:59:53 -0500

    Remove control check on the timing check. Accidentally undercounted by checking control before time. Numbers look more accurate now.

--- misconduct.py
+++ misconduct.py
@@ -71,22 +71,20 @@ def check_control(data_report, sci):
     # Based on control number of misconduct. Look for misconducts within the same hour that have the same number and collapse them.
     control_num_dict = {}
     scis_data = data_report.loc[data_report['institution'] == sci]
-    checked_controls = []
     for index, miscon_entry in scis_data.iterrows():
         control_num = miscon_entry['control_number']
-        if control_num not in checked_controls:
-            checked_controls.append(control_num)
-            # First find all of the misconducts that match the control number
-            filter_control = data_report.loc[data_report['control_number'] == control_num]
-            set_on_date = filter_control.loc[filter_control['misconduct_date'] == miscon_entry['misconduct_date']]
-            #break up list by time range by the hour. If the time is more than an hour ahead or behind count it separately.
-            time_sets = {}
-            for index, miscon in set_on_date.iterrows():
-                time = (miscon['misconduct_time'] // 100) * 100
-                if time in time_sets:
-                    time_sets[time].append((sci, miscon_entry['misconduct_date'], control_num))
-                else:
-                    time_sets[time] = [(sci, miscon_entry['misconduct_date'], control_num)]
+        # First find all of the misconducts that match the control number
+        filter_control = data_report.loc[data_report['control_number'] == control_num]
+        print("Miscon number for control: ", filter_control.size)
+        set_on_date = filter_control.loc[filter_control['misconduct_date'] == miscon_entry['misconduct_date']]
+        #break up list by time range by the hour. If the time is more than an hour ahead or behind count it separately.
+        time_sets = {}
+        for index, miscon in set_on_date.iterrows():
+            time = (miscon['misconduct_time'] // 100) * 100
+            if time in time_sets:
+                time_sets[time].append((sci, miscon_entry['misconduct_date'], control_num))
+            else:
+                 time_sets[time] = [(sci, miscon_entry['misconduct_date'], control_num)]
             print ("Time set: ", time_sets)
             # Should we filter this by a time range? How do we keep track of the same control and date if the misconducts are far apart in the day?
             # Check that entry 'misconduct_time' is within -100 to 100 of the time on the main entry
